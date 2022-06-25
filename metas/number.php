8d68268dab4a0cf00d5f61268423c789924985dc
Octavio Martinez <omartinez@logoscorp.com>
2022-06-25 01:57:10 -0400

    added tag

--- metas/number.php
+++ metas/number.php
@@ -1,4 +1,4 @@
 <?php
-$tag = WP_Rulette::get_var_meta('wp_rulette_tag') ? WP_Rulette::get_var_meta('wp_rulette_tag') : WP_Rulette::get_var_meta('wp_rulette_number');
+$tag = WP_Rulette::get_var_meta('wp_rulette_tag');
 ?>
 <input type="text" class="wp_rulette_tag" id="wp_rulette_tag" name="wp_rulette_tag" value="<?= $tag ?>" />
