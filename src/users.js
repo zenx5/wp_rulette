e8ec70978593ae680e4905f389a2b0e07236ccb0
moises <elmoises.reydereymail.com>
2022-08-02 19:04:38 -0400

    ahora se pueden añadir nuevos usuarios

--- src/users.js
+++ src/users.js
@@ -8,7 +8,6 @@
     		tr.onclick = _ => {
     			sessionStorage.setItem('currentUser', player.id );
     		}
-    		console.log(player)
 			template += `
 	                <td> ${player['name']} </td>
 	                <td> ${player.money} </td>
@@ -16,5 +15,38 @@
 	        tr.innerHTML = template;
         	document.querySelector('.users-container tbody').appendChild(tr);
         };
+    	let tr = document.createElement('tr');
+    	tr.dataset.player = -1;
+    	tr.innerHTML = `
+            <td> <input type="text" class="name" > </td>
+            <td> <input type="number" class="money" > </td>
+    	`
+        document.querySelector('.users-container tbody').appendChild(tr);
+        document.querySelector('.users-container button').onclick = _ => {
+			let players = JSON.parse(sessionStorage.getItem('players') || '[{"id":1,"name":"moises","plays":[],"money":500},{"id":2,"name":"mario","plays":[],"money":400}]');
+			let id = players.sort( (A, B) => B.id - A.id )[0].id + 1;
+			let player = {
+				id: id,
+				name: document.querySelector('.users-container tbody tr input.name').value,
+				money: +document.querySelector('.users-container tbody tr input.money').value
+			}
+			console.log(!player.name , !player.money)
+			console.log(player)
+			if( !player.name || !player.money ) return;
+			players.push( player );
+			sessionStorage.setItem('players', JSON.stringify(players));
+			
+    		let tr2 = document.createElement('tr');
+    		tr2.dataset.player = player.id;
+    		tr2.onclick = _ => {
+    			sessionStorage.setItem('currentUser', player.id );
+    		}
+			let template = `
+	                <td> ${player.name} </td>
+	                <td> ${player.money} </td>
+            `
+	        tr2.innerHTML = template;
+        	document.querySelector('.users-container tbody').insertBefore( tr2, tr );
+        }
     })                
 })();
