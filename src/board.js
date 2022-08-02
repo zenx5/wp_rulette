19f48c8847302cf985fa5ca9a6d2e656e19d9827
Octavio Martinez <omartinez1618@gmail.com>
2022-08-02 11:50:32 -0400

    Update board.js
    validación para verificar cual es el usuario actual y mostrar las jugadas de este, ademas de evitar hacer jugadas sin seleccionar un usuario

--- src/board.js
+++ src/board.js
@@ -1,18 +1,26 @@
 (function() {
     addEventListener('load', function() {
         JSON.parse( sessionStorage.getItem('plays') ).forEach( play => {
-            document.querySelector("[data-tag='"+play.tag+"']").setAttribute('class','board-tag selected')
+            const currentUser = sessionStorage.getItem('current-user') || ''
+            if( play.player === currentUser ){
+                document.querySelector("[data-tag='"+play.tag+"']").setAttribute('class','board-tag selected')
+            }
         })
         document.querySelector('.board-container')
             .addEventListener('click', function(ev) {
+                const currentUser = sessionStorage.getItem('current-user') || ''
                 const target = ev.target;
+                if( currentUser === '' ){
+                    alert('debe seleccionar un usuario')
+                    return;
+                }
                 let plays = JSON.parse( sessionStorage.getItem('plays') ) || []
                 document.querySelectorAll('board-tag').forEach(e => e.setAttribute('class', 'board-tag'));
                 if (target.className === 'board-tag') {
                     target.setAttribute('class', 'board-tag selected');
                     plays.push({
                         tag: target.dataset.tag,
-                        player: 'user'
+                        player: currentUser
                     })
                 }else{
                     target.setAttribute('class', 'board-tag');
