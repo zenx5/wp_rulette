a247ed9875c7252f080a173eb5d27ae3e45f4e33
Octavio Martinez <omartinez1618@gmail.com>
2022-08-06 00:49:15 -0400

    control users

--- src/users.js
+++ src/users.js
@@ -1,56 +1,68 @@
 (function() {
+	const currentUser = parseInt(sessionStorage.getItem("currentUser")) || null;
+	const players = JSON.parse(sessionStorage.getItem('players') || '[]');
+	const plays = JSON.parse(sessionStorage.getItem('plays') || '[]');
+	const createRow = (player) => {
+		const index = players.findIndex(elem => (elem.id===player.id)) + 1
+		const tr = document.createElement('tr');
+		const tdname = document.createElement('td')
+		tdname.innerHTML = player.name
+		tdname.dataset.player = player.id;
+		tdname.addEventListener('click', ev => {
+			document.querySelector('#style-user').innerHTML = "#user-body tr:nth-child("+index+"){font-weight: bold;}"
+			sessionStorage.setItem('currentUser', index );
+			document.querySelectorAll(".board-tag").forEach( elem => {
+				elem.setAttribute("class", "board-tag")
+			});
+			plays.forEach( play => {
+				if( play.player == index ){
+					document.querySelector("[data-tag='"+play.tag+"']").setAttribute('class', 'board-tag selected')	
+				}
+			})
+		})
+		tr.append(tdname)
+		const tdmoney = document.createElement('td')
+		tdmoney.innerHTML = player.money
+		tr.append(tdmoney)
+		const tdaction = document.createElement('td')
+		const button = document.createElement('button')
+		button.innerText = "X"
+		button.dataset.id = index
+		button.addEventListener('click', ev => {
+			const id = ev.target.dataset.id
+			const newPlayers = players.filter(player => player.id != id)
+			sessionStorage.setItem('players', JSON.stringify(newPlayers) )
+			ev.target.parentElement.parentElement.remove()
+		})
+		tdaction.setAttribute('style', 'justify-content: center;align-items: center;display: flex;')
+		tdaction.append(button)
+		tr.append(tdaction)
+		document.querySelector('.users-container tbody').appendChild(tr);		
+	}
+
     addEventListener('load', function() {
-		let players = JSON.parse(sessionStorage.getItem('players') || '[]');
+		if( currentUser ){
+			document.querySelector('#style-user').innerHTML = "#user-body tr:nth-child("+currentUser+"){font-weight: bold;}"
+		}
     	for( let player of players ) {
-			const index = players.findIndex(elem => (elem.id===player.id)) + 1
-			let template = ``
-    		let tr = document.createElement('tr');
-			let tdname = document.createElement('td')
-			tdname.innerHTML = player.name
-			tdname.dataset.player = player.id;
-			tdname.addEventListener('click', ev => {
-				document.querySelector('#style-user').innerHTML = "#user-body tr:nth-child("+index+"){font-weight: bold;}"
-    			sessionStorage.setItem('currentUser', ev.target.dataset.player );
-    		})
-			tr.append(tdname)
-			let tdmoney = document.createElement('td')
-			tdmoney.innerHTML = player.money
-			tr.append(tdmoney)
-        	document.querySelector('.users-container tbody').appendChild(tr);
-        };
-    	let tr = document.createElement('tr');
-    	tr.dataset.player = -1;
-    	tr.innerHTML = `
-            <td> <input type="text" class="name" style="width:100%;"></td>
-            <td> <input type="number" class="money" style="width:100%;"></td>
-    	`
-        document.querySelector('.users-container tbody').appendChild(tr);
-        document.querySelector('.users-container button').onclick = _ => {
-			let players = JSON.parse(sessionStorage.getItem('players') || '[]');
-			players.sort( (A, B) => B.id - A.id );
-			id = players[0] ? players[0].id+1 : 1;
-			let player = {
-				id: id,
-				name: document.querySelector('.users-container tbody tr input.name').value,
-				money: +document.querySelector('.users-container tbody tr input.money').value
-			}
-			document.querySelector('.users-container tbody tr input.name').value = '';
-			document.querySelector('.users-container tbody tr input.money').value = '';
-			if( !player.name || !player.money ) return;
-			players.push( player );
-			sessionStorage.setItem('players', JSON.stringify(players));
-			
-    		let tr2 = document.createElement('tr');
-    		tr2.dataset.player = player.id;
-    		tr2.onclick = _ => {
-    			sessionStorage.setItem('current-user', player.id );
-    		}
-			let template = `
-	                <td> ${player.name} </td>
-	                <td> ${player.money} </td>
-            `
-	        tr2.innerHTML = template;
-        	document.querySelector('.users-container tbody').insertBefore( tr2, tr );
-        }
+			createRow(player)
+        }    	
+		        
+		document
+			.querySelector('.users-container #add')
+			.addEventListener('click', ev => {
+				const name = document.querySelector('.users-container tbody tr input.name').value
+				const money = document.querySelector('.users-container tbody tr input.money').value
+				const player = {
+					id: players[players.length - 1]?.id + 1 || 1,
+					name: name,
+					money: money
+				}
+				players.push(player)
+				sessionStorage.setItem('players', JSON.stringify(players))
+				createRow(player)
+				document.querySelector('.users-container tbody tr input.name').value = ''
+				document.querySelector('.users-container tbody tr input.money').value = ''
+			})
     })                
 })();
