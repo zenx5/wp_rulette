5d5b10a9be7a0627cb3288e64f339977556e48c1
Octavio Martinez <omartinez1618@gmail.com>
2022-08-06 00:50:56 -0400

    reajustes board: cargar jugadas al hacer reload

--- src/board.js
+++ src/board.js
@@ -1,29 +1,34 @@
-(function() {
-    addEventListener('load', function() {
-        ( JSON.parse( sessionStorage.getItem('plays') ) || []).forEach( play => {
-            document.querySelector("[data-tag='"+play.tag+"']").setAttribute('class','board-tag selected')
-        })
-        document.querySelector('.board-container')
-            .addEventListener('click', function(ev) {
-                const currentUser = sessionStorage.getItem('current-user') || ''
-                const target = ev.target;
-                if( currentUser === '' ){
-                    alert('debe seleccionar un usuario')
-                    return;
-                }
-                let plays = JSON.parse( sessionStorage.getItem('plays') ) || []
-                document.querySelectorAll('board-tag').forEach(e => e.setAttribute('class', 'board-tag'));
-                if (target.className === 'board-tag') {
-                    target.setAttribute('class', 'board-tag selected');
-                    plays.push({
-                        tag: target.dataset.tag,
-                        player: currentUser
-                    })
-                }else{
-                    target.setAttribute('class', 'board-tag');
-                    plays = plays.filter( elem => elem.tag !== target.dataset.tag )
-                }
-                sessionStorage.setItem( 'plays', JSON.stringify(plays) );
-            });
-    })                
+(function () {
+    const currentUser = parseInt(sessionStorage.getItem("currentUser")) || null;
+    const plays = JSON.parse(sessionStorage.getItem("plays")) || []
+  addEventListener("load", function () {    
+    plays.forEach((play) => {
+      if (parseInt(play.player) == currentUser) {
+        document
+          .querySelector("[data-tag='" + play.tag + "']")
+          .setAttribute("class", "board-tag selected");
+      }
+    });    
+    document
+      .querySelector(".board-container")
+      .addEventListener("click", function ({target}) {
+        if (currentUser === null) {
+          alert("debe seleccionar un usuario");
+          return;
+        }
+        document.querySelectorAll("board-tag").forEach( elem => (elem.setAttribute("class", "board-tag")));
+
+        if (target.className === "board-tag") {
+          target.setAttribute("class", "board-tag selected");
+          plays.push({
+            tag: target.dataset.tag,
+            player: currentUser
+          });
+        } else {
+          target.setAttribute("class", "board-tag");
+          plays = plays.filter( elem => (elem.tag !== target.dataset.tag));
+        }
+        sessionStorage.setItem("plays", JSON.stringify(plays));
+      });
+  });
 })();
